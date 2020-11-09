79bc3a13962cb00e45540db20ae74a44e8e19575
richbrill <3015680+richbrill@users.noreply.github.com>
2020-11-09 22:48:30 +0000

    Initial Leaflet setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navigation from "./components/Navigation";
 import Sidebar from "./components/Sidebar";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "./assets/main.css";
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
       <Navigation />
       <div className="flex flex-1 w-full h-full">
         <Sidebar />
+        <MapContainer className="flex-grow" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
       </div>
     </div>
   );
